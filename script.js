0a36f89f3d84f02f18f66a2767c950a6f4749727
Daniel Olakotan <horlakz@protonmail.com>
2021-07-08 00:38:11 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+function ageInDays() {
+    var birthYear = prompt("Enter your birth year")
+    var ageInDayss = (2021 - birthYear) * 365;
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You are ' + ageInDayss + ' days old')
+    h1.setAttribute('id', 'ageinDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flexbox-result').appendChild(h1);
+}
+
+
+var elem = document.getElementById("ageInDays")
+elem.innerText = "ageInDayss"
+
+function reset() {
+  var reset_elem = document.getElementById("ageInDays")
+  reset_elem.innerText = ""
+}
