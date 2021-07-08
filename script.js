41cbef8ce259c1cdfcf88ae0bf31dea59a0b915b
Daniel Olakotan <horlakz@protonmail.com>
2021-07-08 03:24:24 +0100

    fixed the reset button and made it reponsive

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ function ageInDays() {
     var ageInDayss = (2021 - birthYear) * 365;
     var h1 = document.createElement('h1');
     var textAnswer = document.createTextNode('You are ' + ageInDayss + ' days old')
-    h1.setAttribute('id', 'ageinDays');
+    h1.setAttribute('id', 'ageInDays');
     h1.appendChild(textAnswer);
     document.getElementById('flexbox-result').appendChild(h1);
 }
 
 
 function reset() {
-    document.getElementById("ageInDays").remove();
+    document.getElementById('ageInDays').remove();
 }
